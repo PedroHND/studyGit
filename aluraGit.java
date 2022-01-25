0c04ebcf8898b71d0d577cd5dd32e77836397ec2
Pedro <nunespedro03@gmail.com>
2022-01-25 18:08:42 -0300

    inserção do teste

--- aluraGit.java
+++ aluraGit.java
@@ -4,7 +4,7 @@ public class aluraGit {
 	public static void main(String[] args) {
 		System.out.println("Verificando se 1 é maior que 0");
 		if(1>0) {
-			
+			System.out.println("teste 123431");
 			System.out.println("O número 1 é maior que o número 0. Assim como qualquer outro número inteiro positivo");
 		}		
 	}
