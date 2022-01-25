f2d1f413429ccd050d3936a0d598c85ccd63f33c
Pedro <nunespedro03@gmail.com>
2022-01-25 16:43:02 -0300

    alteracao do texto antes da função 2.0

--- aluraGit.java
+++ aluraGit.java
@@ -2,7 +2,7 @@
 public class aluraGit {
 
 	public static void main(String[] args) {
-		System.out.println("Comparando se 1 é maior que 0");
+		System.out.println("Verificando se 1 é maior que 0");
 		if(1>0) {
 			
 			System.out.println("1 é maior que 0");
